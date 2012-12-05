5554c5299efc10803ce1f5c80fe29934b4b904df
wsky <wskyhx@gmail.com>
2012-12-05 23:02:28 +0800

    add c# websocket tests

---
+++ tests/dotnet/WebSockeSample/Controllers/WebSocketHandler.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebSockeSample.Controllers
+{
+    public class WebSocketHandler : IHttpHandler
+    {
+        /// <summary>
+        /// You will need to configure this handler in the Web.config file of your 
+        /// web and register it with IIS before being able to use it. For more information
+        /// see the following link: http://go.microsoft.com/?linkid=8101007
+        /// </summary>
+        #region IHttpHandler Members
+
+        public bool IsReusable
+        {
+            // Return false in case your Managed Handler cannot be reused for another request.
+            // Usually this would be false in case you have some state information preserved per request.
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.IsWebSocketRequest)
+                context.AcceptWebSocketRequest(this.HandleWebSocket);
+            else
+                context.Response.StatusCode = 400;
+        }
+
+        #endregion
+
+        private async Task HandleWebSocket(WebSocketContext context)
+        {
+            var maxMessageSize = 1024;
+            var receiveBuffer = new byte[maxMessageSize];
+            var socket = context.WebSocket;
+
+            var total = 0;
+            
+            while (socket.State == WebSocketState.Open)
+            {
+                var ret = await socket.ReceiveAsync(new ArraySegment<byte>(receiveBuffer), CancellationToken.None);
+
+                if (ret.MessageType == WebSocketMessageType.Close)              
+                    await socket.CloseAsync(WebSocketCloseStatus.NormalClosure
+                        , string.Empty
+                        , CancellationToken.None);
+                else if (ret.MessageType == WebSocketMessageType.Binary)
+                    await socket.CloseAsync(WebSocketCloseStatus.InvalidMessageType
+                        , "Cannot accept binary frame"
+                        , CancellationToken.None);
+                else
+                {
+                    var count = ret.Count;
+                    while (!ret.EndOfMessage)
+                    {
+                        if (count >= maxMessageSize)
+                        {
+                            await socket.CloseAsync(WebSocketCloseStatus.MessageTooBig
+                                , string.Format("Maximum message size: {0} bytes.", maxMessageSize)
+                                , CancellationToken.None);
+                            return;
+                        }
+
+                        ret = await socket.ReceiveAsync(new ArraySegment<byte>(receiveBuffer, count, maxMessageSize - count), CancellationToken.None);
+                        count += ret.Count;
+                    }
+
+                    var receivedString = Encoding.UTF8.GetString(receiveBuffer, 0, count);
+
+                    if (total == 0)
+                    {
+                        total = int.Parse(receivedString);
+                        continue;
+                    }
+
+                    var outputBuffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes(receivedString));
+                    var w = new Stopwatch();
+                    w.Start();
+                    for (var i = 0; i < total; i++)
+                        await socket.SendAsync(outputBuffer, WebSocketMessageType.Text, true, CancellationToken.None);
+                    w.Stop();
+                    Console.WriteLine(w.ElapsedMilliseconds);
+                }
+            }
+        }
+    }
+}
---
+++ tests/dotnet/WebSocketTest/UnitTest1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text;
+using System.Diagnostics;
+
+namespace WebSocketTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private int _counter;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var total = 10000;
+            var msg = "hello";
+            var client = new ClientWebSocket();
+            client.ConnectAsync(new Uri("ws://localhost:8889/ws.axd"), CancellationToken.None).Wait();
+            client.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(total.ToString())), WebSocketMessageType.Text, true, CancellationToken.None).Wait();
+            client.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(msg)), WebSocketMessageType.Text, true, CancellationToken.None).Wait();
+
+            var w = new Stopwatch();
+            w.Start();
+            ReceiveMessages(client, msg, total).Wait();
+            w.Stop();
+            Trace.WriteLine(w.ElapsedMilliseconds);
+
+            while (_counter < total) Thread.Sleep(10);
+        }
+
+        private async Task ReceiveMessages(ClientWebSocket socket,string msg, int total)
+        {
+            var maxMessageSize = 1024;
+            var receiveBuffer = new byte[maxMessageSize];
+
+            for (var i = 0; i < 10; i++)
+            {
+                var ret = await socket.ReceiveAsync(new ArraySegment<byte>(receiveBuffer), CancellationToken.None);
+
+                if (ret.MessageType != WebSocketMessageType.Text) continue;
+
+                var count = ret.Count;
+                while (!ret.EndOfMessage)
+                {
+                    ret = await socket.ReceiveAsync(new ArraySegment<byte>(receiveBuffer, count, maxMessageSize - count), CancellationToken.None);
+                    count += ret.Count;
+                }
+
+                var receivedString = Encoding.UTF8.GetString(receiveBuffer, 0, count);
+                Trace.WriteLine(receivedString);
+                Interlocked.Increment(ref _counter);
+            }
+        }
+    }
+}
