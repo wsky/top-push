e317f1bf3888e47f91662137a23a27e9b1876782
wsky <wskyhx@gmail.com>
2013-04-08 11:15:44 +0800

    update script

--- scripts/server-install.sh
+++ scripts/server-install.sh
@@ -1,5 +1,6 @@
 #jetty
 JETTY_VERSION=8.1.7.v20120910
 rm -rf jetty*
-wget http://download.eclipse.org/jetty/$JETTY_VERSION/dist/jetty-distribution-$JETTY_VERSION.tar.gz
+#wget http://download.eclipse.org/jetty/$JETTY_VERSION/dist/jetty-distribution-$JETTY_VERSION.tar.gz
+wget http://archive.eclipse.org/jetty/$JETTY_VERSION/dist/jetty-distribution-$JETTY_VERSION.tar.gz
 tar xfz jetty-distribution-$JETTY_VERSION.tar.gz
