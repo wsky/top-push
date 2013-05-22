b6c5360e021ac8431315e4da4df6c62a01e273c6
houkun <houkun@taobao.com>
2013-05-22 15:44:17 +0800

    update pushmanager usage

---
+++ samples/top-link-support/src/main/java/com/taobao/top/mix/FlowControl.java
@@ -0,0 +1,8 @@
+package com.taobao.top.mix;
+
+import com.taobao.top.link.endpoint.Identity;
+import com.taobao.top.link.schedule.Scheduler;
+
+public class FlowControl extends Scheduler<Identity> {
+
+}
--- samples/top-link-support/src/main/java/com/taobao/top/mix/MixServer.java
+++ samples/top-link-support/src/main/java/com/taobao/top/mix/MixServer.java
@@ -1,20 +1,17 @@
 package com.taobao.top.mix;
 
 import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import com.taobao.top.link.Logger;
 import com.taobao.top.link.channel.ServerChannelSender;
 import com.taobao.top.link.channel.websocket.WebSocketServerChannel;
 import com.taobao.top.link.endpoint.Endpoint;
 import com.taobao.top.link.endpoint.EndpointProxy;
 import com.taobao.top.link.endpoint.StateHandler;
 import com.taobao.top.link.schedule.Scheduler;
-import com.taobao.top.push.Client;
-import com.taobao.top.push.ClientConnection;
-import com.taobao.top.push.ClientStateHandler;
 import com.taobao.top.push.Identity;
 import com.taobao.top.push.PushManager;
 
@@ -30,42 +27,26 @@ public class MixServer {
 	public static void start(int port) {
 		// whatever, log first
 		ServerLoggerFactory loggerFactory = new ServerLoggerFactory();
+		Logger logger = loggerFactory.create(MixServer.class);
 
 		// for push support
 		pushManager = new PushManager(loggerFactory, 50000,
 				100,// care about senderCount by cpu usage
-				1000,//sender idle 
 				1000);
-		pushManager.setClientStateHandler(new ClientStateHandler() {
-			@Override
-			public void onClientPending(Client client) {
-			}
-
-			@Override
-			public void onClientOffline(Client client) {
-			}
-
-			@Override
-			public void onClientIdle(Client client) {
-			}
-
-			@Override
-			public void onClientDisconnect(Client client, ClientConnection clientConnection) {
-			}
-
-			@Override
-			public Identity onClientConnecting(Map<String, String> headers) throws Exception {
-				return new ClientIdentity(headers.get("appkey"));
-			}
-		});
+		// pushManager.setClientStateHandler(null);
+		// pushManager.setMessageStateHandler(messageStateHandler);
+		logger.info("==== top-push init.");
 
 		// init base server
 		// scheduler for business process and loadbalance/attack-prevent
-		scheduler = new Scheduler<com.taobao.top.link.endpoint.Identity>();
+		scheduler = new FlowControl();
 		scheduler.setUserMaxPendingCount(100);
 		// biz-threadpool
-		scheduler.setThreadPool(new ThreadPoolExecutor(20, 300, 300, TimeUnit.SECONDS, new SynchronousQueue<Runnable>()));
+		scheduler.setThreadPool(new ThreadPoolExecutor(20,
+				200,// max threadpool size
+				300, TimeUnit.SECONDS, new SynchronousQueue<Runnable>()));
 		scheduler.start();
+		logger.info("==== scheduler(%s) start.", scheduler.getClass().getName());
 
 		// extended low-level channelHandler
 		ServerEndpointChannelHandler channelHandler = new ServerEndpointChannelHandler(loggerFactory);
@@ -73,27 +54,22 @@ public class MixServer {
 		channelHandler.setStateHandler(new StateHandler() {
 			@Override
 			public void onConnected(EndpointProxy endpoint, ServerChannelSender sender) {
-				// build pushClient here, and will call to onClientConnecting
-				try {
-					Map<String, String> headers = new HashMap<String, String>();
-					headers.put("appkey", endpoint.getIdentity().toString());
-					pushManager.connectClient(headers, new ClientEndpointConnection(endpoint, sender));
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+				// build pushClient here
+				pushManager.connectClient(
+						(ClientIdentity) endpoint.getIdentity(),
+						new ClientEndpointConnection(endpoint, sender));
 			}
 		});
-
 		// your server for talking
 		serverEndpoint = new Endpoint(loggerFactory, new ServerIdentity());
 		serverEndpoint.setChannelHandler(channelHandler);
 		serverEndpoint.setMessageHandler(new ServerMessageHandler());
-
 		// websocket serverchannel
 		WebSocketServerChannel serverChannel = new WebSocketServerChannel(loggerFactory, port, true);
 		// if you want SSL/TLS
 		// serverChannel.setSSLContext(sslContext);
 		serverEndpoint.bind(serverChannel);
+		logger.info("==== mix-server bind at port %s", port);
 	}
 
 	public static void stop() throws InterruptedException {
