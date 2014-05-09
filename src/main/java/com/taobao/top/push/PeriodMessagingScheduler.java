7190f7c8b12085cff58ba130b55b132923f39bd0
houkun <houkun@taobao.com>
2014-05-09 17:49:02 +0800

    #47;impl period scheduler

--- src/main/java/com/taobao/top/push/PeriodMessagingScheduler.java
+++ src/main/java/com/taobao/top/push/PeriodMessagingScheduler.java
@@ -1,9 +1,61 @@
 package com.taobao.top.push;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Queue;
+import java.util.Random;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class PeriodMessagingScheduler implements MessagingScheduler {
+	private static Random RANDOM = new Random();
+
+	private Set<Client> pendingClients;
+
+	private Timer timer;
+	private TimerTask task;
+
+	private ExecutorService executor;
+
+	public PeriodMessagingScheduler() {
+		this.pendingClients = new ConcurrentSkipListSet<Client>();
+
+		this.setPeriod(100);
+
+		this.executor = new ThreadPoolExecutor(
+				Runtime.getRuntime().availableProcessors(),
+				Runtime.getRuntime().availableProcessors() * 2,
+				30, TimeUnit.SECONDS,
+				new SynchronousQueue<Runnable>(),
+				Executors.defaultThreadFactory(),
+				new ThreadPoolExecutor.CallerRunsPolicy());
+	}
+
+	public void setPeriod(int value) {
+		if (this.timer == null)
+			this.timer = new Timer(this.getClass().getSimpleName(), true);
+
+		if (this.task != null)
+			this.task.cancel();
+
+		this.timer.purge();
+		this.timer.schedule(this.task = new TimerTask() {
+			@Override
+			public void run() {
+				dispatch();
+			}
+		}, value, value);
+	}
+
 	public void schedule(Client client, MessagingTask messaging) {
 		Queue<MessagingTask> queue = this.getQueue(client);
 		if (queue == null) {
@@ -13,6 +65,57 @@ public class PeriodMessagingScheduler implements MessagingScheduler {
 							queue = new ArrayBlockingQueue<MessagingTask>(10000));
 			}
 		}
+		if (!queue.offer(messaging))
+			messaging.run();
+		else
+			this.pendingClients.add(client);
+	}
+
+	protected void dispatch() {
+		Set<Client> temp = this.pendingClients;
+		this.pendingClients = new ConcurrentSkipListSet<Client>();
+		List<Client> clients = new ArrayList<Client>(temp);
+		// avoid not fair between clients
+		Collections.shuffle(clients, RANDOM);
+
+		for (Client client : clients) {
+			try {
+				this.execute(client);
+			} catch (Exception e) {
+				// log
+			}
+		}
+	}
+
+	protected void execute(final Client client) {
+		this.executor.submit(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					flush(client);
+				} catch (Exception e) {
+					// log
+				}
+			}
+		});
+	}
+
+	protected void flush(Client client) {
+		Queue<MessagingTask> queue = this.getQueue(client);
+		MessagingTask task = null;
+
+		while ((task = queue.poll()) != null) {
+			try {
+				if (!this.isCompleted(task.execute()))
+					break;
+			} catch (Exception e) {
+				// FIXME log
+			}
+		}
+	}
+
+	protected boolean isCompleted(MessagingStatus status) {
+		return status == MessagingStatus.SENT || status == MessagingStatus.ABORT;
 	}
 
 	@SuppressWarnings("unchecked")
