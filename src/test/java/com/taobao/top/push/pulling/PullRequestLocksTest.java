906036bd9e5883fe92abf4f828fe2d6100023b9f
houkun <houkun@taobao.com>
2014-05-12 16:04:15 +0800

    add threaded test for locks

--- src/test/java/com/taobao/top/push/pulling/PullRequestLocksTest.java
+++ src/test/java/com/taobao/top/push/pulling/PullRequestLocksTest.java
@@ -2,6 +2,9 @@ package com.taobao.top.push.pulling;
 
 import static org.junit.Assert.*;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.junit.Test;
 
 import com.taobao.top.push.Client;
@@ -11,20 +14,40 @@ public class PullRequestLocksTest {
 
 	@Test
 	public void acquire_test() {
-		PullRequestLocks pendings = new PullRequestLocks();
-		assertTrue(pendings.acquire(client, 1));
-		assertFalse(pendings.acquire(client, 1));
-		pendings.release(client, 1);
-		assertTrue(pendings.acquire(client, 1));
+		PullRequestLocks locks = new PullRequestLocks();
+		assertTrue(locks.acquire(client, 1));
+		assertFalse(locks.acquire(client, 1));
+		locks.release(client, 1);
+		assertTrue(locks.acquire(client, 1));
 	}
 
 	@Test
 	public void timeout_test() throws InterruptedException {
-		PullRequestLocks pendings = new PullRequestLocks();
-		pendings.setTimeout(100);
-		assertTrue(pendings.acquire(client, 1));
-		assertFalse(pendings.acquire(client, 1));
+		PullRequestLocks locks = new PullRequestLocks();
+		locks.setTimeout(100);
+		assertTrue(locks.acquire(client, 1));
+		assertFalse(locks.acquire(client, 1));
 		Thread.sleep(500);
-		assertTrue(pendings.acquire(client, 1));
+		assertTrue(locks.acquire(client, 1));
+	}
+
+	@Test
+	public void threaded_test() throws InterruptedException {
+		int total = 10;
+		final CountDownLatch latch = new CountDownLatch(total);
+		final AtomicInteger count = new AtomicInteger();
+		final PullRequestLocks locks = new PullRequestLocks();
+		for (int i = 0; i < total; i++) {
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					if (locks.acquire(client, 1))
+						count.incrementAndGet();
+					latch.countDown();
+				}
+			}).start();
+		}
+		latch.await();
+		assertEquals(1, count.get());
 	}
 }
