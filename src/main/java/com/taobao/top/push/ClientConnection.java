9e52f21d86baaf562be3fe84d7fdca0d49d5561f
houkun <houkun@taobao.com>
2014-05-12 16:09:30 +0800

    update connection

--- src/main/java/com/taobao/top/push/ClientConnection.java
+++ src/main/java/com/taobao/top/push/ClientConnection.java
@@ -1,17 +1,18 @@
 package com.taobao.top.push;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 public abstract class ClientConnection {
 	private Object id;
 	private Object clientId;
-	private Map<?, ?> headers;
+	private Map<Object, Object> headers;
 	private Date connectTime;
 
 	public ClientConnection(Object id, Map<?, ?> headers) {
 		this.id = id;
-		this.headers = headers;
+		this.headers = new HashMap<Object, Object>(headers);
 		this.connectTime = new Date();
 	}
 
@@ -27,8 +28,12 @@ public abstract class ClientConnection {
 		return this.id;
 	}
 
-	public Map<?, ?> getHeaders() {
-		return this.headers;
+	public Map<Object, Object> getHeaders() {
+		return new HashMap<Object, Object>(this.headers);
+	}
+
+	public Object getHeader(Object key) {
+		return this.headers.get(key);
 	}
 
 	public Date getConnectTime() {
